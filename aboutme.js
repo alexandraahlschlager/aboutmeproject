22556dc46bcf07747b05dc985a297e9ae7ce8aa2
alexandraahlschlager <52722852+alexandraahlschlager@users.noreply.github.com>
2019-07-10 18:25:52 -0700

    Create aboutme.js

---
+++ aboutme.js
@@ -0,0 +1,30 @@
+function changeImage()
+{
+document.images["jsbutton"].src= "https://i.pinimg.com/originals/82/4f/84/824f84d100e5189a6b9d6607362772f1.jpg" height="300";
+return true;
+}
+function changeImageBack()
+{
+ document.images["jsbutton"].src = "https://66.media.tumblr.com/fb99de4cdfeafa2066a08149093a2435/tumblr_oxirm34F471w8f1tco1_250.jpg";
+ return true;
+}
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+	var dropdowns = document.getElementsByClassName("dropdown-content");
+	var i;
+	for (i = 0; i < dropdowns.length; i++) {
+  	var openDropdown = dropdowns[i];
+  	if (openDropdown.classList.contains('show')) {
+    	openDropdown.classList.remove('show');
+  	}
+	}
+  }
+}
