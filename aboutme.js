b4ec21dc626930331f18805884ca221a68ca1c48
alexandraahlschlager <52722852+alexandraahlschlager@users.noreply.github.com>
2019-07-10 18:33:13 -0700

    Update aboutme.js

--- aboutme.js
+++ aboutme.js
@@ -1,6 +1,6 @@
 function changeImage()
 {
-document.images["jsbutton"].src= "https://i.pinimg.com/originals/82/4f/84/824f84d100e5189a6b9d6607362772f1.jpg" height="300";
+document.images["jsbutton"].src= "https://i.pinimg.com/originals/82/4f/84/824f84d100e5189a6b9d6607362772f1.jpg";
 return true;
 }
 function changeImageBack()
